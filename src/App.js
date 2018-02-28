8ea4d24f89266b6ad24c1ac246e46e2adb223068
Amidou Kante <kamdiou@gmail.com>
2018-02-28 13:15:31 -0500

    able to change button sizes

--- src/App.js
+++ src/App.js
@@ -7,63 +7,66 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container buttons">
+          <Button>
+            Button
+          </Button>
           <div className="row small">
+            <Button color='red' size="small">
+              Button
+            </Button>
+            <Button color='orange' size="small">
+              Button
+            </Button>
+            <Button color='yellow' size="small">
+              Button
+            </Button>
+            <Button color='olive' size="small">
+              Button
+            </Button>
           </div>
           <div className="row medium">
+            <Button color='green' size="medium">
+              Button
+            </Button>
+            <Button color='teal' size="medium">
+              Button
+            </Button>
+            <Button color='blue' size="medium">
+              Button
+            </Button>
+            <Button color='violet' size="medium">
+              Button
+            </Button>
           </div>
           <div className="row large">
+            <Button color='purple' size="large">
+              Button
+            </Button>
+            <Button color='pink' size="large">
+              Button
+            </Button>
           </div>
           <div className="row wide">
+            <Button color='brown' size="wide">
+              Button
+            </Button>
+            <Button color='grey' size="wide">
+              Button
+            </Button>
           </div>
           <div className="row extra-wide">
+            <Button color='black' size="extra-wide">
+              Button
+            </Button>
+            <Button color='red' size="extra-wide">
+              Button
+            </Button>
           </div>
           <div className="row full-width">
+            <Button color='white' size="full-width">
+              Button
+            </Button>
           </div>
-          <Button>
-            Click Me!
-          </Button>
-          <Button color='red' size="small">
-            Click Me!
-          </Button>
-          <Button color='orange' size="medium">
-            Click Me!
-          </Button>
-          <Button color='yellow'>
-            Click Me!
-          </Button>
-          <Button color='olive'>
-            Click Me!
-          </Button>
-          <Button color='green'>
-            Click Me!
-          </Button>
-          <Button color='teal'>
-            Click Me!
-          </Button>
-          <Button color='blue'>
-            Click Me!
-          </Button>
-          <Button color='violet'>
-            Click Me!
-          </Button>
-          <Button color='purple'>
-            Click Me!
-          </Button>
-          <Button color='pink'>
-            Click Me!
-          </Button>
-          <Button color='brown'>
-            Click Me!
-          </Button>
-          <Button color='grey'>
-            Click Me!
-          </Button>
-          <Button color='black'>
-            Click Me!
-          </Button>
-          <Button color='white'>
-            Click Me!
-          </Button>
         </div>
       </div>
     );
