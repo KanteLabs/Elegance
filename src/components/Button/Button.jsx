9b6fe85b02e8f9a3cb968c1402f01644ef286ac8
Amidou Kante <kamdiou@gmail.com>
2018-02-28 01:48:35 -0500

    created options file, and button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import {
+    COLORS,
+    FLAT_COLOR
+} from '../../options'
 
 class Button extends Component {
+    static propTypes = {
+        
+        // A button can have various colors
+        color: PropTypes.oneOf([
+            ...COLORS,
+            'red'
+        ]),
+        
+    }
+    
+    convertColor = () => {
+        const { color } = this.props;
+        return FLAT_COLOR[color];
+    }
+    
     render(){
+        console.log(this.props)
+        const style = {
+            background: this.convertColor(),
+            width: '100px',
+            height: '100px'
+        }
         return(
-            <div>
-                
+            <div style={style}>
+
             </div>
         )
     }
---
+++ src/options/config.js
@@ -0,0 +1,33 @@
+export const COLORS = [
+    'red',
+    'orange',
+    'yellow',
+    'olive',
+    'green',
+    'teal',
+    'blue',
+    'violet',
+    'purple',
+    'pink',
+    'brown',
+    'grey',
+    'black',
+    'white'
+]
+
+export const FLAT_COLOR = {
+    'red': '#f44336',
+    'orange': '#F08A5D',
+    'yellow': '',
+    'olive': '',
+    'green': '',
+    'teal': '',
+    'blue': '',
+    'violet': '',
+    'purple': '',
+    'pink': '',
+    'brown': '',
+    'grey': '',
+    'black': '#222831',
+    'white': '#fcfcfc'
+}
